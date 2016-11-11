6ed87b5ce63379799e347ac888ac7a3aeccede1c
CWS-Pektin <werner.elijah@gmail.com>
2016-11-11 22:37:09 +0100

    Revert "Unity Project"
    This reverts commit 76a31e141fab5cad609738fb4c304a8eb4bf6c9d.

---
+++ DroidTestingGame/Assets/Scripts/AccelerometerMove.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AccelerometerMove : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		float temp = Input.acceleration.x;
+		transform.Translate (0, 0, temp);
+	}
+}
---
+++ DroidTestingGame/Assets/Scripts/Touch.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Touch : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if (Input.touchCount > 0) {
+			//touch controls
+				
+
+		}
+	//Multitouch check
+	/*	if (Input.touchCount > 0 && Input.touchCount < 2) {
+			Debug.Log ("Nun befindet sich " + Input.touchCount + " Finger auf den Display.");
+			return;
+		} else if (Input.touchCount > 1) {
+			Debug.Log ("Nun befinden sich " + Input.touchCount + " Finger auf dem Display.");
+			return;
+		}
+	*/}
+}
